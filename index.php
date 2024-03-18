0820500c939bdf97e6b66381c0f2a025da7f86e8
unknown <pandey.ravi9800@gmail.com>
2024-03-18 18:59:14 +0530

    more modifications

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
                                         ?>
                                         <p style="font-size: 10px; color:black;"><?php echo ucwords($catrun['cast']); ?></p>
                                         <div class="text-center">
-                                            <a class="btn btn-primary" href="service-list.php?sid=<?php echo $catrun['id']; ?>">Apply Now</a>
+                                            <a class="btn btn-primary" href="service.php?catid=<?php echo $catrun['id']; ?>">Apply Now</a>
                                         </div>
                                     </div>
                                 </div>
@@ -55,7 +55,7 @@
     </div>
 </section>
 
-<section class="explore-section section-padding" id="section_2">
+<section class="explore-section section-padding" style="background-color: white;" id="section_2">
     <div class="container">
         <div class="col-12 text-center">
             <h2 class="mb-4">Learn TAX</h2>
@@ -194,10 +194,10 @@
                     <div class="row">
 
                         <div class="col-lg-12 col-12 text-center">
-                            <h2 class="mb-5">Get in touch</h2>
+                            <h2>Get in touch</h2>
                         </div>
 
-                        <div class="col-lg-5 col-12 mb-4 mb-lg-0">
+                        <div class="col-lg-5 col-12 mb-lg-0">
                             <iframe class="google-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2595.065641062665!2d-122.4230416990949!3d37.80335401520422!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80858127459fabad%3A0x808ba520e5e9edb7!2sFrancisco%20Park!5e1!3m2!1sen!2sth!4v1684340239744!5m2!1sen!2sth" width="100%" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                         </div>
 
@@ -226,7 +226,7 @@
                         </div>
 
                         <div class="col-lg-3 col-md-6 col-12 mx-auto">
-                            <h4 class="mb-3">Connect With Us</h4>
+                            <h4 class="">Connect With Us</h4>
 
                             <i class="bi bi-facebook"></i> || <i class="bi bi-instagram"></i>
                         </div>
